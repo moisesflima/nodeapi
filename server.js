9a32795b2913542a0b7dbfab1fd23ee9fe7af1c6
moisesflima <moisesflima@gmail.com>
2019-08-27 14:32:36 -0300

    fim modulo nodjs

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 // iniciando o app
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 // iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 requireDir('./src/models');
-const Product = mongoose.model("Product");
 
-// Primeira rota
-app.get('/',(req,res)=>{
-    Product.create({
-        title: "react-native",
-        description: "Build native apps with React",
-        url: "http://github.com/facebook/react-native",
-    });
-
-
-    res.send('Hello Rocketseat!');
-});
+// Rotas
+app.use('/api',require('./src/routes'));
 
 app.listen(3001);
